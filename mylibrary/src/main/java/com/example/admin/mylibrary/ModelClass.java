fd224cf3e06f61e564a59cd6e0272b44c7372d81
team_sanganan <kumartannu2009@gmail.com>
2016-01-07 17:06:05 +0530

    intial commit

---
+++ mylibrary/src/main/java/com/example/admin/mylibrary/ModelClass.java
@@ -0,0 +1,15 @@
+package com.example.admin.mylibrary;
+
+/**
+ * Created by ADMIN on 07-Jan-16.
+ */
+public class ModelClass {
+
+    public String display()
+    {
+
+        String name="sanganan";
+        return name;
+
+    }
+}
